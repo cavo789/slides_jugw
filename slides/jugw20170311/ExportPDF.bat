606eefaf4201902970f4e5e1645f5d045089df39
Christophe Avonture <christophe@avonture.be>
2017-03-09 08:14:55 +0100

    Initial upload - JUGW 20170311

---
+++ slides/jugw20170311/ExportPDF.bat
@@ -0,0 +1,4 @@
+REM Export reveal.js -> PDF
+REM Read C:\Christophe\Repository\misc\Documents\Development\reveal.md for better understanding
+
+c:\christophe\tools\decktape\phantomjs.exe c:\christophe\tools\decktape\decktape.js reveal index.html "jugw20170311.pdf"
