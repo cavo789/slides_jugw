3b475a4aa93136d4c2024da0c755df062c9436a7
Christophe <cavo789@yahoo.com>
2018-04-05 22:01:59 +0200

    JUGW

---
+++ markdown/jugw.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="robots" content="index, follow" />
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8;" />
+		<meta name="author" content="JUG Wallonie" />
+		<meta name="designer" content="JUG Wallonie" />
+		<meta name="keywords" content="%TITLE%" />
+		<meta name="description" content="%TITLE%" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+		<meta property="og:url" content="%URL%" />
+		<meta property="og:type" content="article" />
+		<meta property="og:image" content="%URL_IMG%" />
+		<meta property="og:image:width" content="1200" />
+		<meta property="og:image:height" content="522" />
+		<meta property="og:title" content="%TITLE%" />
+		<meta property="og:site_name" content="Comptes-rendus | www.jugwallonie.be" />
+		<meta property="og:description" content="%TITLE%" />
+
+		<title>%TITLE%</title>
+
+		<link href= "data:image/x-icon;base64,AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAPv/AAAA/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAREiIAAAAAERESIiAAAAARERIiIgAAABEREiIiAAAAERESIiIAAAARERIiIgAAABEREiIiAAAAERESIiIAAAARERIiIgAAABEREiIiAAAAERESIiIAAAARERIiIgAAABEREiIiAAAAERESIiIAAAAQAAAAIgAAAAAAAAACB+BwAAOAMAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAIP5AADH/QAA" rel="shortcut icon" type="image/vnd.microsoft.icon"/>
+
+		<link media="screen" rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap.min.css" />
+		<link media="screen" rel="stylesheet" type="text/css" href="libs/font-awesome/css/font-awesome.min.css" />
+		<link media="screen" rel="stylesheet" type="text/css" href="assets/css/markdown.css" />
+
+	</head>
+
+	<body>
+
+		<div class="container">
+			<article id="top">
+				%CONTENT%
+				<footer class="hidden-xs hidden-sm">
+					<img src="https://slides.jugwallonie.be/img/logo.png" width="424" height="65" alt="JUG Wallonie"/>
+					<ul class="fa-ul list-inline">
+						<li><i class="fa-li fa fa-home"></i><a href="https://jugwallonie.be">jugwallonie.be</a></li>
+						<li><i class="fa-li fa fa-facebook-official"></i><a href="https://www.facebook.com/groups/joomla.belgique" target="_blank" rel="nofollow noopener noreferrer">Joomla! User Group Wallonie</a></li>
+						<li><i class="fa-li fa fa-desktop"></i><a href="https://slides.jugwallonie.be" target="_blank" rel="noopener noreferrer">slides.jugwallonie.be</a></li>
+					</ul>
+				</footer>
+			</article>
+		</div>
+		<script type="text/javascript" src="libs/jquery/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/markdown.js"></script>
+
+	</body>
+
+</html>
